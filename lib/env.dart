c7177fc7a167e95d49a18c0dfd91c50cd091d347
Myat Min Thant <>
2025-08-19 14:56:32 +0700

    just check previous commits for baseURL!!

--- lib/env.dart
+++ lib/env.dart
@@ -2,4 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
   static String get apiKey => dotenv.env['API_KEY'] ?? '';
+  static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
 }
--- lib/services/service_dio.dart
+++ lib/services/service_dio.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_clean/env.dart';
 import 'package:injectable/injectable.dart';
 
 @module
@@ -7,7 +8,7 @@ abstract class ServiceDio {
   Dio dio() {
     final dio = Dio();
 
-    dio.options.baseUrl = "https://newsapi.org/v2";
+    dio.options.baseUrl = Env.baseUrl;
     dio.options.connectTimeout = const Duration(seconds: 10);
 
     return dio;
